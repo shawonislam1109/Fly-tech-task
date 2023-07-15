140ef1eafa264dc3825b16dff178932c51ebb5b4
Your Name <you@example.com>
2023-07-15 22:19:24 +0600

    fare summary

--- src/Component/FlightDetails/Summary/Summary.js
+++ src/Component/FlightDetails/Summary/Summary.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React from "react";
 
 const Summary = () => {
-    return (
-        <div>
-            <h1>This is the Summary </h1>
-        </div>
-    );
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead className="bgcolor text-slate-200 ">
+            <tr>
+              <th>Pax Type</th>
+              <th>Base Fare (BDT)</th>
+              <th>Tax (BDT)</th>
+              <th>Total (BDT)</th>
+              <th>PX Count </th>
+              <th>Service Fee (BDT)</th>
+              <th>Sub Total (BDT)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr className="bg-green-200">
+              <th>Adult x1</th>
+              <td>4090 tk</td>
+              <td>275 tk</td>
+              <td>4815 tk</td>
+              <td>1</td>
+              <td>0 BDT</td>
+              <td>4815 tk</td>
+            </tr>
+            {/* row 2 */}
+            <tr>
+              <th>Child x1</th>
+              <td>4090 tk</td>
+              <td>275 tk</td>
+              <td>4815 tk</td>
+              <td>1</td>
+              <td>0 BDT</td>
+              <td>4815 tk</td>
+            </tr>
+            {/* row 3 */}
+            <tr className="bg-green-200">
+              <th>Infant x1</th>
+              <td>4090 tk</td>
+              <td>275 tk</td>
+              <td>4815 tk</td>
+              <td>1</td>
+              <td>0 BDT</td>
+              <td>4815 tk</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Summary;
--- src/Route/Route.js
+++ src/Route/Route.js
@@ -1,6 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import MainHome from "../Component/Main/MainHome";
-import FlightDetails from "../Component/FlightDetails/FlightDetails";
 import Home from "../Component/Home/Home";
 import Flight from "../Component/FlightDetails/Flight/Flight";
 import Policy from "../Component/FlightDetails/Policy/Policy";
